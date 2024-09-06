0be22de017493ed9fdcccf4317f38c4f289f1402
Andrzej Kozlowski <andrzej@decktech.eu>
2024-09-06 21:30:49 +0000

    first build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Welcome to andy v3.0</h1>");
+  response.end("<h1> Welcome to andy v1.0 from CICD</h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
